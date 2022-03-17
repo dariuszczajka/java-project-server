2ece1f37f0f473a5c2123e069bbc03cfcfa3baf5
darek <dczajka@tuta.io>
2022-03-17 21:48:33 +0100

    tomcat-jdbc dependency

--- src/main/java/com/czajkadariusz/controller/RowController.java
+++ src/main/java/com/czajkadariusz/controller/RowController.java
@@ -6,15 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+
 
 @Controller
 public class RowController {
     @Autowired
     private RowService rowService;
 
+    @CrossOrigin
     @GetMapping(value = "/api/rows")
     public ResponseEntity<List<Row>> getRows(){
         List<Row> listRow = rowService.findAllRows();
@@ -22,6 +25,7 @@ public class RowController {
         return new ResponseEntity<>(listRow, HttpStatus.OK);
     }
 
+    @CrossOrigin
     @GetMapping(value = "/api/rows/repeating")
     public ResponseEntity<List<Row>> getRepeatingRows(){
         List<Row> listRow = rowService.findRepeating();
@@ -29,6 +33,7 @@ public class RowController {
         return new ResponseEntity<>(listRow, HttpStatus.OK);
     }
 
+    @CrossOrigin
     @GetMapping(value = "/api/rows/distinct")
     public ResponseEntity<List<Row>> getDistinctRows(){
         List<Row> listRow = rowService.findDistinct();
