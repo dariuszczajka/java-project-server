cdcfbf529da038feece96d19e35ce54c5db0cba0
darek <dczajka@tuta.io>
2022-03-17 18:08:01 +0100

    initial commit

---
+++ src/main/java/com/czajkadariusz/CzajkadariuszApplication.java
@@ -0,0 +1,13 @@
+package com.czajkadariusz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CzajkadariuszApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CzajkadariuszApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/czajkadariusz/CzajkadariuszApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czajkadariusz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CzajkadariuszApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
